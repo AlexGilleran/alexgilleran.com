43d55a729e9129ce15e8d9ecf93094a56cecab2b
Alex Gilleran <alex.gilleran@gmail.com>
2013-01-17 15:51:55 +1100

    Changed the design around a bit, still just boxes.

--- alexgilleran.com/js/main.js
+++ alexgilleran.com/js/main.js
@@ -2,14 +2,14 @@ $(document).ready(resizeScreen);
 $(window).resize(resizeScreen);
 
 function resizeScreen() {
-	var mainNav = $('#left-column');
-	var navHeight = mainNav.height() - 20;
-	var linkSideLength = navHeight / 4 - 20;
+	var mainNav = $('#main-nav');
+	var navHeight = mainNav.height() - 40;
+	var linkSideLength = navHeight / 8 - 20;
 	var mainNavWidth = linkSideLength + 40
 	
 	$('.link').height(linkSideLength).width(linkSideLength);
 	
-	
 	mainNav.width(mainNavWidth);
-	$('#right-column').width($('#wrapper').width() - mainNavWidth); 
+	
+	$('#content').height($('body').height() - 40);
 }
